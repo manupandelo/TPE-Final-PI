6c32591b56ab6bc15e2b1e4fc09680b2b8b428a3
TizianoAmetller <Tizianoametller@gmail.com>
2024-07-04 19:39:41 -0300

    seventh commit

--- parkingTicketsADT.c
+++ parkingTicketsADT.c
@@ -13,17 +13,33 @@ typedef struct agencyNode{
     struct agencyNode * tail;
 }agencyNode;
 
-typedef struct agency * agencyList;
+typedef struct agencyNode * agencyList;
+
+typedef struct plateNode{
+    char plate[11];
+    int cant;
+    struct plateNode * tail;
+}plateNode;
+
+typedef struct plateNode * plateList;
+
+typedef struct infraction{
+    plateList first;
+}infraction;
+
 
 struct parkingTicketsCDT
 {
     /*QUERY 1*/
     int * infractionsAmm;
     /*QUERY 1*/
+
     /*QUERY 2*/
     agencyList first;
     /*QUERY 2*/
+    /*QUERY 3*/
+    infraction * infractionPlate;
+    /*QUERY 3*/
 };
 
-parkingTicketsADT newADT()
-
+void 
