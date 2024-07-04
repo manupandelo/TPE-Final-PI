a6fe3b5395a424381bfcb20274e859481f298cb9
TizianoAmetller <Tizianoametller@gmail.com>
2024-07-04 19:13:11 -0300

    fourth commit

--- parkingTicketsADT.c
+++ parkingTicketsADT.c
@@ -7,11 +7,23 @@
 #include <string.h>
 #include "parkingTicketsADT.h"
 
+typedef struct agencyNode{
+    int * infractions;
+    char issuingAgency[35];
+    struct agencyNode * tail;
+}agencyNode;
+
+typedef struct agency * agencyList;
+
 struct parkingTicketsCDT
 {
     /*QUERY 1*/
     int * infractionsAmm;
     /*QUERY 1*/
+
+    /*QUERY 2*/
+    agencyList first;
+    /*QUERY 2*/
 };
 
 void 
